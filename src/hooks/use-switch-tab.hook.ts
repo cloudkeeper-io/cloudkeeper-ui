c238c5652ed56776812d98fbd0874b9c22fad14b
edraitsev <ddrrai@gmail.com>
2019-03-25 01:24:28 +0100

    Added react-hooks/exhaustive-deps to avoid eslint --fix problem

--- src/hooks/use-switch-tab.hook.ts
+++ src/hooks/use-switch-tab.hook.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react'
 
 export function useSwitchTab(trigger: any, tabsAmount: number, initialState = 0): [number, (newTab: number) => void] {
