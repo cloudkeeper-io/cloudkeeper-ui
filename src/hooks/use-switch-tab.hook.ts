04b8442161aa8fa8f0a3342ea86dd6890380bf5c
edraitsev <ddrrai@gmail.com>
2019-03-25 19:48:58 +0100

    Changed switchTab hook logic a bit

--- src/hooks/use-switch-tab.hook.ts
+++ src/hooks/use-switch-tab.hook.ts
@@ -1,11 +1,10 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react'
 
 export function useSwitchTab(trigger: any, tabsAmount: number, initialState = 0): [number, (newTab: number) => void] {
   const [currentTab, setTab] = useState(initialState - 1)
 
   useEffect(() => {
-    setTab(currentTab + 1 >= tabsAmount ? 0 : currentTab + 1)
+    setTab(tab => (tab + 1 >= tabsAmount ? 0 : tab + 1))
   }, [tabsAmount, trigger])
 
   return [currentTab, setTab]
