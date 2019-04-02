7508861732ae2f4c2e91d1004fabaa9d017738f3
edraitsev <ddrrai@gmail.com>
2019-04-03 00:51:30 +0200

    fixed gradient issue

--- src/styles/svg.defs.tsx
+++ src/styles/svg.defs.tsx
@@ -3,19 +3,19 @@ import React from 'react'
 export default () => (
   <svg style={{ width: 0, height: 0 }}>
     <defs>
-      <linearGradient id="lightLine" x1="0" y1="0" x2="1" y2="0">
+      <linearGradient id="lightLine" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse">
         <stop offset="0%" stopColor="#FFAA6E" />
         <stop offset="100%" stopColor="#FF5F99" />
       </linearGradient>
-      <linearGradient id="lightLineSecondary" x1="0" y1="0" x2="1" y2="0">
+      <linearGradient id="lightLineSecondary" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse">
         <stop offset="0%" stopColor="#2FF6F5" />
         <stop offset="100%" stopColor="#0470FE" />
       </linearGradient>
-      <linearGradient id="darkLine" x1="0" y1="0" x2="1" y2="0">
+      <linearGradient id="darkLine" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse">
         <stop offset="0%" stopColor="#B9FFEC" />
         <stop offset="100%" stopColor="#B9FFEC" />
       </linearGradient>
-      <linearGradient id="darkLineSecondary" x1="0" y1="0" x2="1" y2="0">
+      <linearGradient id="darkLineSecondary" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse">
         <stop offset="0%" stopColor="#DB60FF" />
         <stop offset="100%" stopColor="#DB60FF" />
       </linearGradient>
