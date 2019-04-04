e24febec99dfe05f85d3da6571af3388d0e66c35
Arseny Yankovsky <arseny.yankovsky@emarketeer.com>
2019-04-04 19:12:12 +0200

    Added gtm and made a setup process nicer

--- src/head.tsx
+++ src/head.tsx
@@ -8,20 +8,18 @@ interface HeadProps {
   theme: any
 }
 
-export default memo(withTheme(({ theme }: HeadProps) => {
-  return (
-    <Helmet>
-      <title>Cloud Keeper</title>
-      <meta name="theme-color" content={theme.colors.background} data-react-helmet="true" />
-      <link rel="shortcut icon" href={theme.favicon} />
-      <script>{
+export default memo(withTheme(({ theme }: HeadProps) => (
+  <Helmet>
+    <title>Cloud Keeper</title>
+    <meta name="theme-color" content={theme.colors.background} data-react-helmet="true" />
+    <link rel="shortcut icon" href={theme.favicon} />
+    <script>{
         `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','${getConfig().gtmContainer}');`
       }
-      </script>
-    </Helmet>
-  )
-}))
+    </script>
+  </Helmet>
+)))
