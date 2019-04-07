8b2d21317206c2300b3c1531b3680c65f33b95c9
Arseny Yankovsky <arseny.yankovsky@emarketeer.com>
2019-04-07 18:03:28 +0200

    Moved config to react_env

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,1 +1,17 @@
-export const getConfig = () => (window as any).env
+const env = process.env.REACT_APP_ENV || 'dev'
+
+const config: any = {
+  dev: {
+    authUrl: 'https://dev-api.cloudkeeper.io/auth',
+    apolloUri: 'https://dev-api.cloudkeeper.io/graphql',
+    gtmContainer: 'GTM-MNL9CG8',
+  },
+  prod: {
+    authUrl: 'https://api.cloudkeeper.io/auth',
+    apolloUri: 'https://api.cloudkeeper.io/graphql',
+    gtmContainer: 'GTM-59RXFDW',
+  },
+}
+
+
+export const getConfig = () => config[env]
