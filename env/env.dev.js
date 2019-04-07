7686040149354361302a2e562801ff00b6f6e7cb
Arseny Yankovsky <arseny.yankovsky@emarketeer.com>
2019-04-07 18:06:40 +0200

    Moved config to react_env

--- env/env.dev.js
+++
@@ -1,5 +0,0 @@
-window.env = {
-  authUrl: 'https://dev-api.cloudkeeper.io/auth',
-  apolloUri: 'https://dev-api.cloudkeeper.io/graphql',
-  gtmContainer: 'GTM-MNL9CG8',
-}
--- env/env.prod.js
+++
@@ -1,5 +0,0 @@
-window.env = {
-  authUrl: 'https://api.cloudkeeper.io/auth',
-  apolloUri: 'https://api.cloudkeeper.io/graphql',
-  gtmContainer: 'GTM-59RXFDW',
-}
