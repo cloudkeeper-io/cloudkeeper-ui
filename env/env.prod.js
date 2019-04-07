1a0709d5ddbe5904ffe295c33a35bb6d42c9e1cc
Arseny Yankovsky <arseny.yankovsky@emarketeer.com>
2019-04-07 17:38:18 +0200

    Added correct prod config

--- env/env.prod.js
+++ env/env.prod.js
@@ -1,5 +1,5 @@
 window.env = {
-  authUrl: 'https://dev-api.cloudkeeper.io/auth',
-  apolloUri: 'https://sc3u8ugbt4.execute-api.eu-central-1.amazonaws.com/dev',
+  authUrl: 'https://api.cloudkeeper.io/auth',
+  apolloUri: 'https://api.cloudkeeper.io/graphql',
   gtmContainer: 'GTM-59RXFDW',
 }
