ddcd34a530b548088edd6ad0ddbddb60daa3dc76
edraitsev <ddrrai@gmail.com>
2019-04-08 19:11:29 +0200

    Added new version of schema

--- src/graphql/mutations/tenants.mutation.ts
+++ src/graphql/mutations/tenants.mutation.ts
@@ -1,21 +1,21 @@
 import gql from 'graphql-tag'
 
-export const createTenant = gql`    
-    mutation createTenant($name: String!, $accessKey: String!, $secretKey: String!, $region: String!) {
-        createTenant(name: $name, accessKey: $accessKey, secretKey: $secretKey, region: $region) {
-            id
-            name
-            isSetupCompleted
-        }
+export const createTenant = gql`
+  mutation createTenant($name: String!, $accessKey: String!, $secretKey: String!, $region: String!) {
+    createTenant(name: $name, accessKey: $accessKey, secretKey: $secretKey, region: $region) {
+      id
+      name
+      isSetupCompleted
     }
+  }
 `
 
 export const removeTenant = gql`
-    mutation removeTenant($id: String!) {
-        deleteTenant(id: $id) {
-            id
-            name
-            isSetupCompleted
-        }
+  mutation removeTenant($id: String!) {
+    deleteTenant(id: $id) {
+      id
+      name
+      isSetupCompleted
     }
+  }
 `
--- src/graphql/queries/aws-regions.query.ts
+++ src/graphql/queries/aws-regions.query.ts
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag'
 
 export const awsRegionsQuery = gql`{
-    awsRegions
+  awsRegions
 }
 `
