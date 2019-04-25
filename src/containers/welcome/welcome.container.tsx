77e5c0234bfb5a8c9096caf4186ad974f0ddf0ab
ayankovsky <p44gr10@gmail.com>
2019-04-25 19:54:43 +0200

    Added welcome page

--- src/containers/welcome/welcome.container.tsx
+++ src/containers/welcome/welcome.container.tsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components/macro'
 import { RouteComponentProps, withRouter } from 'react-router-dom'
 
 import Card from '../../components/card.component'
 import { AccentText, Title, Text } from '../../components/typography.component'
-import CreateTenantModal from "../settings/components/tenants/create-tenant-modal.component";
-import Button from "../../components/button/button.component";
+import CreateTenantModal from '../settings/components/tenants/create-tenant-modal.component'
+import Button from '../../components/button/button.component'
 
 
 const Wrapper = styled.div`
@@ -39,24 +39,29 @@ interface WelcomeContainerProps extends RouteComponentProps {
 }
 
 const Welcome = ({ className }: WelcomeContainerProps) => {
-    const [isCreateModalOpen, setCreateModalOpen] = useState(false)
+  const [isCreateModalOpen, setCreateModalOpen] = useState(false)
 
-    return (
-        <Wrapper className={className}>
-            <Content>
-                <Title>Welcome to Cloudkeeper!</Title>
-                <Section>
-                    <Text>You don't have any projects yet. To start using the cloudkeeper create your first project.</Text><br/>
-                    <Text>After that we'll guide you through a simple setup process which will take around <AccentText>2 minutes</AccentText> and will require <AccentText>no code changes.</AccentText></Text>
-                </Section>
-                <AddButton onClick={() => setCreateModalOpen(true)}>Create Project</AddButton>
-            </Content>
-            <CreateTenantModal
-                onClose={() => setCreateModalOpen(false)}
-                isOpen={isCreateModalOpen}
-            />
-        </Wrapper>
-    );
+  return (
+    <Wrapper className={className}>
+      <Content>
+        <Title>Welcome to Cloudkeeper!</Title>
+        <Section>
+          <Text>
+              You don&apos;t have any projects yet. To start using the cloudkeeper create your first project.
+          </Text>
+          <br />
+          <Text>After that we&apos;ll guide you through a simple setup process which will take around
+            <AccentText>2 minutes</AccentText> and will require <AccentText>no code changes.</AccentText>
+          </Text>
+        </Section>
+        <AddButton onClick={() => setCreateModalOpen(true)}>Create Project</AddButton>
+      </Content>
+      <CreateTenantModal
+        onClose={() => setCreateModalOpen(false)}
+        isOpen={isCreateModalOpen}
+      />
+    </Wrapper>
+  )
 }
 
 export default withRouter<WelcomeContainerProps>(Welcome)
