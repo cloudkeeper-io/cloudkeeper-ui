3256b87fdbdefb64203986e3dba1083c74042593
ayankovsky <p44gr10@gmail.com>
2019-04-25 19:41:07 +0200

    Added welcome page

---
+++ src/containers/welcome/welcome.container.tsx
@@ -0,0 +1,62 @@
+import React, {useState} from 'react'
+import styled from 'styled-components/macro'
+import { RouteComponentProps, withRouter } from 'react-router-dom'
+
+import Card from '../../components/card.component'
+import { AccentText, Title, Text } from '../../components/typography.component'
+import CreateTenantModal from "../settings/components/tenants/create-tenant-modal.component";
+import Button from "../../components/button/button.component";
+
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: calc(100% - 40px);
+  min-height: calc(100vh - 100px);
+  padding: 0 20px;
+  align-items: center;
+  justify-content: flex-start;
+  @media(max-width: 800px) {
+    padding: 0;
+  }
+`
+const Content = styled(Card)`
+  padding: 20px;
+  width: 750px;
+`
+
+const Section = styled.div`
+  margin: 20px 0 ;
+`
+
+const AddButton = styled(Button)`
+  margin: 15px auto 0 auto;
+`
+
+interface WelcomeContainerProps extends RouteComponentProps {
+    className?: string
+}
+
+const Welcome = ({ className }: WelcomeContainerProps) => {
+    const [isCreateModalOpen, setCreateModalOpen] = useState(false)
+
+    return (
+        <Wrapper className={className}>
+            <Content>
+                <Title>Welcome to Cloudkeeper!</Title>
+                <Section>
+                    <Text>You don't have any projects yet. To start using the cloudkeeper create your first project.</Text><br/>
+                    <Text>After that we'll guide you through a simple setup process which will take around <AccentText>2 minutes</AccentText> and will require <AccentText>no code changes.</AccentText></Text>
+                </Section>
+                <AddButton onClick={() => setCreateModalOpen(true)}>Create Project</AddButton>
+            </Content>
+            <CreateTenantModal
+                onClose={() => setCreateModalOpen(false)}
+                isOpen={isCreateModalOpen}
+            />
+        </Wrapper>
+    );
+}
+
+export default withRouter<WelcomeContainerProps>(Welcome)
