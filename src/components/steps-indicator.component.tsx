92c7b148b5bf0ab24fa9ca634b08db5f67d93bf8
edraitsev <ddrrai@gmail.com>
2019-07-13 20:51:26 +0200

    Fixed step indicator styles

--- src/components/steps-indicator.component.tsx
+++ src/components/steps-indicator.component.tsx
@@ -1,16 +1,8 @@
-/* eslint-disable no-nested-ternary */
 import React from 'react'
 import styled from 'styled-components/macro'
 import { transparentize } from 'polished'
 import times from 'lodash/times'
 import noop from 'lodash/noop'
-
-const getBackground = (p: any) => {
-  if (p.active) {
-    return p.color || p.theme.colors.primary
-  }
-  return p.color ? transparentize(0.6, p.color) : p.theme.controls.color
-}
 
 const getBoxShadow = (p: any) => {
   if (p.active) {
@@ -24,6 +16,18 @@ const Wrapper = styled.div`
   align-items: center;
   box-sizing: initial;
 `
+const Circle = styled.div<{ active: boolean, color?: string }>`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+  background: ${p => (p.color ? transparentize(0.6, p.color) : p.theme.controls.color)};
+  opacity: ${p => (p.active ? 0 : 1)};
+  transform: translate(-50%,-50%);
+  transition: all 0.5s ease-in-out 0s;
+`
 const Step = styled.div<{ active: boolean, cursor: string }>`
   position: relative;
   width: ${p => (p.active ? '24px' : '6px')};
@@ -36,8 +40,9 @@ const Step = styled.div<{ active: boolean, cursor: string }>`
 const StepLine = styled.div<{ active: boolean, color?: string }>`
   height: 6px;
   border-radius: 6px;
-  background: ${p => getBackground(p)};
+  background: ${p => p.color || p.theme.controls.activeGradient};
   box-shadow: ${p => getBoxShadow(p)};
+  opacity: ${p => (p.active ? 1 : 0)};
   transition: all 0.5s ease-in-out 0s;
 `
 
@@ -54,6 +59,7 @@ export default ({ steps, index, className, color, onClick = noop }: StepsIndicat
     {times(steps, i => (
       <Step key={i} active={i === index} onClick={() => onClick(i)} cursor={onClick === noop ? '' : 'pointer'}>
         <StepLine active={i === index} color={color} />
+        <Circle active={i === index} color={color} />
       </Step>
     ))}
   </Wrapper>
