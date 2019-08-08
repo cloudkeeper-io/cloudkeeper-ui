d5695fcd34954890019781001fa6136e8b4f7bf6
Arseny Yankovsky <ArsenyYankovsky@users.noreply.github.com>
2019-08-08 22:47:10 +0200

    Fix dates in tables (#19)
    * Added fixes to tables
    
    * Added fixes to tables
    
    * Added fixes to tables

--- src/components/data-page-header/data-page-header.component.tsx
+++ src/components/data-page-header/data-page-header.component.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Typography } from '@material-ui/core'
 import moment, { Moment } from 'moment'
 
-import { DatepickerWrapper, HeaderContainer, PredefinedDate } from './data-page-hader.styles'
+import { DatepickerWrapper, HeaderContainer, PredefinedDate } from './data-page-header.styles'
 import { DateRangePicker } from '../date-range-picker'
 import { DateRange } from '../datepicker/datepicker.component'
 
@@ -15,8 +15,19 @@ interface DataPageHeaderProps {
 
 export const DataPageHeader = ({ title, startDate, endDate, onDateRangeChanged }: DataPageHeaderProps) => {
   const today = moment()
-  const sixDaysAgo = moment().subtract(6, 'days')
-  const startOfMonth = moment().startOf('month')
+
+  const sevenDaysAgo = moment().subtract(6, 'days').startOf('day')
+  const startOfMonth = moment().startOf('month').startOf('day')
+
+  const normalizedStartDate = moment(startDate!).startOf('day')
+  const normalizedEndDate = moment(endDate!).endOf('day')
+
+  const startOfToday = moment(today).startOf('day')
+  const endOfToday = moment(today).endOf('day')
+
+  const isTodaySelected = startOfToday.isSame(normalizedStartDate) && endOfToday.isSame(normalizedEndDate)
+  const isLast7DaysSelected = sevenDaysAgo.isSame(normalizedStartDate) && endOfToday.isSame(endDate!)
+  const isThisMonthSelected = startOfMonth.isSame(startDate!) && endOfToday.isSame(endDate!)
 
   return (
     <HeaderContainer>
@@ -26,19 +37,19 @@ export const DataPageHeader = ({ title, startDate, endDate, onDateRangeChanged }
       <DatepickerWrapper>
         <PredefinedDate
           onClick={() => onDateRangeChanged({ startDate: today, endDate: today })}
-          active={today.isSame(startDate!, 'day') && today.isSame(endDate!, 'day')}
+          active={isTodaySelected}
         >
           Today
         </PredefinedDate>
         <PredefinedDate
-          onClick={() => onDateRangeChanged({ startDate: sixDaysAgo, endDate: today })}
-          active={sixDaysAgo.isSame(startDate!, 'day') && today.isSame(endDate!, 'day')}
+          onClick={() => onDateRangeChanged({ startDate: sevenDaysAgo, endDate: today })}
+          active={isLast7DaysSelected}
         >
             Last 7 days
         </PredefinedDate>
         <PredefinedDate
           onClick={() => onDateRangeChanged({ startDate: startOfMonth, endDate: today })}
-          active={startOfMonth.isSame(startDate!, 'day') && today.isSame(endDate!, 'day')}
+          active={isThisMonthSelected}
         >
             This Month
         </PredefinedDate>
