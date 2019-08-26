104458d4ea3a8162baf777a2a89dca3743f131b9
edraitsev <ddrrai@gmail.com>
2019-08-26 15:29:17 +0200

    Added missed generated type

---
+++ src/graphql/mutations/types/generateTawkUserHash.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: generateTawkUserHash
+// ====================================================
+
+export interface generateTawkUserHash {
+  generateTawkUserHash: string | null;
+}
